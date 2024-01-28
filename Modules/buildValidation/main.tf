aa26524c41b22bef75d0d178ec4e3b53eca242f8
AmitGujar <amithero3342@gmail.com>
2024-01-28 21:35:01 +0530

    minimum reviewers added

--- Modules/buildValidation/main.tf
+++ Modules/buildValidation/main.tf
@@ -8,13 +8,13 @@ terraform {
 }
 
 # creating repository on Azure DevOps project
-resource "azuredevops_git_repository" "new_repo" {
-  project_id = var.project_id
-  name       = "development"
-  initialization {
-    init_type = "Clean"
-  }
-}
+# resource "azuredevops_git_repository" "new_repo" {
+#   project_id = var.project_id
+#   name       = "development"
+#   initialization {
+#     init_type = "Clean"
+#   }
+# }
 
 # creating build definition on Azure DevOps project
 resource "azuredevops_build_definition" "build_definition" {
@@ -55,15 +55,41 @@ resource "azuredevops_branch_policy_build_validation" "build_validation" {
     # this scope defines the branches that will be validated whose name starts with dev
     scope {
       # repository_id  = azuredevops_git_repository.new_repo.id
-      repository_id  = var.repository_id
+      repository_id  = null
       repository_ref = "refs/heads/dev"
       match_type     = "Prefix"
     }
 
     # this scope defines the main branch in all repos will be validated
     # this scope is also act as a inheritance scope
-    # scope {
-    #   match_type = "DefaultBranch"
-    # }
+    scope {
+      match_type = "DefaultBranch"
+    }
+  }
+}
+
+# it will apply minmium number of reviewers required to approve a pull request
+resource "azuredevops_branch_policy_min_reviewers" "min_reviewers" {
+  project_id = var.project_id
+
+  enabled  = true
+  blocking = true
+
+  settings {
+    reviewer_count                         = 1 #minimum reviewers required to approve
+    submitter_can_vote                     = false
+    last_pusher_cannot_approve             = true
+    allow_completion_with_rejects_or_waits = false
+
+    scope {
+      repository_id  = var.repository_id
+      repository_ref = var.repository_ref_branch
+      match_type     = "Exact"
+    }
+
+    # inheritance scope
+    scope {
+      match_type = "DefaultBranch"
+    }
   }
 }
